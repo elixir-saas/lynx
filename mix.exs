6ecd44aedad89d885a55e2369432d19e800ca19b
Justin Tormey <justin@upsidefinancing.com>
2022-09-22 14:32:40 -0500

    Update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Lynx.MixProject do
     [
       app: :lynx,
       version: "0.2.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       description: description(),
