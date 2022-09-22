a81209145572dbeb2f7c95b5885483cba83abf31
Justin Tormey <justin@upsidefinancing.com>
2022-09-22 14:31:31 -0500

    Fix tests

--- test/lynx_test.exs
+++ test/lynx_test.exs
@@ -7,7 +7,7 @@ defmodule LynxTest do
     test "inserts a link into a binary" do
       assert Lynx.HTML.linkify_text("go to www.example.com!") == [
         "go to ",
-        {:safe, [?<, "a", [[?\s, "href", ?=, ?", "http://www.example.com", ?"]], ?>, "www.example.com", ?<, ?/, "a", ?>]},
+        {:safe, [?<, "a", [?\s, "href", ?=, ?", "http://www.example.com", ?"], ?>, "www.example.com", ?<, ?/, "a", ?>]},
         "!"
       ]
     end
@@ -15,30 +15,30 @@ defmodule LynxTest do
     test "inserts multiple links into a binary" do
       assert Lynx.HTML.linkify_text("go to www.example.com and test.com/some/path") == [
         "go to ",
-        {:safe, [?<, "a", [[?\s, "href", ?=, ?", "http://www.example.com", ?"]], ?>, "www.example.com", ?<, ?/, "a", ?>]},
+        {:safe, [?<, "a", [?\s, "href", ?=, ?", "http://www.example.com", ?"], ?>, "www.example.com", ?<, ?/, "a", ?>]},
         " and ",
-        {:safe, [?<, "a", [[?\s, "href", ?=, ?", "http://test.com/some/path", ?"]], ?>, "test.com/some/path", ?<, ?/, "a", ?>]}
+        {:safe, [?<, "a", [?\s, "href", ?=, ?", "http://test.com/some/path", ?"], ?>, "test.com/some/path", ?<, ?/, "a", ?>]}
       ]
     end
 
     test "adds link attributes" do
       assert Lynx.HTML.linkify_text("www.example.com", link_attrs: [target: "_blank", rel: "noopener noreferrer"]) == [
         {:safe, [?<, "a", [
-          [?\s, "href", ?=, ?", "http://www.example.com", ?"],
-          [?\s, "rel", ?=, ?", "noopener noreferrer", ?"],
-          [?\s, "target", ?=, ?", "_blank", ?"]
+          ?\s, "href", ?=, ?", "http://www.example.com", ?",
+          ?\s, "rel", ?=, ?", "noopener noreferrer", ?",
+          ?\s, "target", ?=, ?", "_blank", ?"
         ], ?>, "www.example.com", ?<, ?/, "a", ?>]}
       ]
     end
 
     test "processes the link href" do
       assert Lynx.HTML.linkify_text("www.example.com", process_href: & "#{elem(&1, 1)}#anchor")
-        == [{:safe, [?<, "a", [[?\s, "href", ?=, ?", "http://www.example.com#anchor", ?"]], ?>, "www.example.com", ?<, ?/, "a", ?>]}]
+        == [{:safe, [?<, "a", [?\s, "href", ?=, ?", "http://www.example.com#anchor", ?"], ?>, "www.example.com", ?<, ?/, "a", ?>]}]
     end
 
     test "processes the link text" do
       assert Lynx.HTML.linkify_text("www.example.com", process_text: & "(#{elem(&1, 1)})")
-        == [{:safe, [?<, "a", [[?\s, "href", ?=, ?", "http://www.example.com", ?"]], ?>, "(www.example.com)", ?<, ?/, "a", ?>]}]
+        == [{:safe, [?<, "a", [?\s, "href", ?=, ?", "http://www.example.com", ?"], ?>, "(www.example.com)", ?<, ?/, "a", ?>]}]
     end
   end
 end
