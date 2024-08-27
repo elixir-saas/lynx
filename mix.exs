c10ee62d1735d6f7de94018cb1528a7222af361d
Justin Tormey <jrtormey@gmail.com>
2024-08-27 09:39:30 -0500

    v0.1.0

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule Lynx.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :lynx,
-      version: "0.2.0",
+      version: @version,
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
