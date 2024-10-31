edcf8240e5d0f4438de56bdfd608ad0ab82172a6
Justin Tormey <jrtormey@gmail.com>
2024-10-31 12:37:36 -0500

    Fix default link pattern for two-character tlds

--- lib/lynx/parser.ex
+++ lib/lynx/parser.ex
@@ -15,9 +15,9 @@ defmodule Lynx.Parser do
 
   def strategy(type), do: {type, pattern(type)}
 
-  def pattern(:link),
-    do:
-      ~r{(?<url>(?:https?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~%:\/?#[\]@!\$&'\(\)\*\+,;=.]+[\w\-_\/?&\+])}u
+  def pattern(:link) do
+    ~r/(?<url>(?:https?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-_~%:\/?#[\]@!\$&'\(\)\*\+,;=]*[\w\-_\/?&\+])/u
+  end
 
   def pattern(:mention), do: ~r{@\w+}u
   def pattern(:hashtag), do: ~r{#\w+}u
